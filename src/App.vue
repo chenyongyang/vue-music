2661dcc7f18c02ef38640dbb8adb55e06e72462d
ChenYongYang <1214137301@qq.com>
2018-10-14 21:48:18 +0800

    添加推荐页tb组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <header></header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+import Header from 'components/header'
+  import Tab from 'components/tab'
+  export default {
+    name: 'app',
+    components: {
+      Header,
+      Tab
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Recommend from 'pages/recommend'
+import Singer from 'pages/singer'
+import Search from 'pages/search'
+import Rank from 'pages/rank'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+  routes: [{
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: '/recommend',
+    component: Recommend
+  },
+  {
+    path: '/singer',
+    component: Singer
+  },
+  {
+    path: '/search',
+    component: Search
+  },
+  {
+    path: '/rank',
+    component: Rank
+  }
   ]
 })
