aa0e4ecbcfdb611b455ceb70f506e3353f37a4df
ChenYongYang <1214137301@qq.com>
2018-10-17 16:05:23 +0800

    fastclick解决移动端300ms延迟

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
 
 import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 import axios from './axios'
 Vue.use(axios)
